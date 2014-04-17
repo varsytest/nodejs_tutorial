65e7c53ba192b2184d45afc8135b83edfd59d7b3
Andrey Sizov <andrey.sizov@jetbrains.com>
2014-04-17 23:24:52 +0400

    Added handlers.

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+   console.log("Request handler 'start' was started");
+}
+
+function upload() {
+   console.log("Request handler 'upload' was started");
+}
+
+exports.start = start;
+exports.upload = upload;
